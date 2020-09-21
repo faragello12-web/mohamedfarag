a7ca982cc2580a0b712edcac61d8ee8a76bc6bd3
faragello12-web <faragello12-web@users.noreply.github.com>
2020-09-22 00:34:39 +0200

    portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleID, navID) => {
+  const toggle = document.getElementById(toggleID),
+    nav = document.getElementById(navID);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+/*===== SCROLL SECTIONS ACTIVE LINK*/
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+/*SCROLL HOME*/
+sr.reveal(".home__title", {});
+sr.reveal(".home__theme", { delay: 200 });
+
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about__img", { delay: 500 });
+sr.reveal(".about__subtitle", { delay: 300 });
+sr.reveal(".about__profession", { delay: 400 });
+sr.reveal(".about__text", { delay: 500 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", { delay: 200 });
+sr.reveal(".skills__data", { interval: 200 });
+sr.reveal(".skills__img", { delay: 400 });
+/*SCROLL PORTFOLIO*/
+sr.reveal(".portfolio__img", { interval: 200 });
+/*SCROLL CONTACT*/
+sr.reveal(".contact__subtitle", {});
+sr.reveal(".contact__text", { interval: 200 });
+sr.reveal(".contact__input", { delay: 400 });
+sr.reveal(".contact__button", { delay: 600 });
+// themes
+let theme = localStorage.getItem("theme");
+
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option clicked:", mode);
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
+
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+
+  localStorage.setItem("theme", mode);
+}
